7bafd10c0ff532838a52d6e6ded6c9017c0b1b0a
rahilcsit <rahilcsit@gmail.com>
2025-01-28 23:30:30 +0530

    Assignment solved

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+        RWOnly rwOnly=new RWOnly();
+    //    rwOnly.name="Rahil";
+    //    System.out.println(rwOnly.name);//the field is not visible or the field is private.
+
+    rwOnly.setName("Rahil");
+    System.out.println(rwOnly.getName());
+
+    }
   
 }
